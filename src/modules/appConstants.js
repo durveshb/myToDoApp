57849ff3b7d9034b635e88636ede69d4fedaf796
Durvesh Bhalekar <durvesh.5.db@gmail.com>
2020-09-17 19:40:44 +0530

    fix relative paths for images

--- src/modules/appConstants.js
+++ src/modules/appConstants.js
@@ -1,19 +1,29 @@
 export const filters = [
-  { filId: "fil-ug-dec", src: "./../images/urgLow.svg" },
-  { filId: "fil-ug-inc", src: "./../images/urgHigh.svg" },
-  { filId: "fil-cg-per", src: "./../images/catPersonal.svg" },
-  { filId: "fil-cg-aca", src: "./../images/catAcademic.svg" },
-  { filId: "fil-cg-soc", src: "./../images/catSocial.svg" },
+  { filId: "fil-ug-dec", src: "./images/urgLow.svg" },
+  { filId: "fil-ug-inc", src: "./images/urgHigh.svg" },
+  { filId: "fil-cg-per", src: "./images/catPersonal.svg" },
+  { filId: "fil-cg-aca", src: "./images/catAcademic.svg" },
+  { filId: "fil-cg-soc", src: "./images/catSocial.svg" },
 ];
 
 export const urgencyLevels = [
-    {filId:"ug-1", name : "Low", value : "1", src : "./../images/urgLow.svg"},
-    {filId:"ug-2", name : "Medium", value : "2", src : "./../images/urgMedium.svg"},
-    {filId:"ug-3", name : "High", value : "3", src : "./../images/urgHigh.svg"}
-]
+  { filId: "ug-1", name: "Low", value: "1", src: "./images/urgLow.svg" },
+  { filId: "ug-2", name: "Medium", value: "2", src: "./images/urgMedium.svg" },
+  { filId: "ug-3", name: "High", value: "3", src: "./images/urgHigh.svg" },
+];
 
 export const categories = [
-    {filId:"ct-1", name : "Personal", value : "1", src : "./../images/catPersonal.svg"},
-    {filId:"ct-2", name : "Academic", value : "2", src : "./../images/catAcademic.svg"},
-    {filId:"ct-3", name : "Social", value : "3", src : "./../images/catSocial.svg"}
-]
+  {
+    filId: "ct-1",
+    name: "Personal",
+    value: "1",
+    src: "./images/catPersonal.svg",
+  },
+  {
+    filId: "ct-2",
+    name: "Academic",
+    value: "2",
+    src: "./images/catAcademic.svg",
+  },
+  { filId: "ct-3", name: "Social", value: "3", src: "./images/catSocial.svg" },
+];
